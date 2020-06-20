5df8e146590e9f2db7df1d2325cf4c7def18e8bb
Sukritha <ssukrithas@gmail.com>
2020-06-20 23:57:52 +0530

    first commit

---
+++ src/test/java/qaclickacademy/Mavenjava/RestapiTest.java
@@ -0,0 +1,18 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class RestapiTest {
+
+	@Test
+	public void RestapiTest1()
+	{
+		System.out.println("RestapiTest1");
+	}
+	
+	@Test
+	public void RestapiTest2()
+	{
+		System.out.println("RestapiTest2");
+	}
+}
---
+++ src/test/java/qaclickacademy/Mavenjava/SeleniumJavaTest.java
@@ -0,0 +1,19 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class SeleniumJavaTest {
+
+	@Test
+	public void SelTest1()
+	{
+		System.out.println("SelTest1");
+	}
+	
+	@Test
+	public void SelTest2()
+	{
+		System.out.println("SelTest2");
+	}
+	
+}
